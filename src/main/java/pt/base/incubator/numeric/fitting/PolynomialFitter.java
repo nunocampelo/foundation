33a9797d4653d243c2ed219f05c0ca148a1c19a1
campelo <campelo.nuno@gmail.com>
2018-06-11 22:12:35 +0100

    changed api from polynomial fitter to use lists instead of arrays

--- src/main/java/pt/base/incubator/numeric/fitting/PolynomialFitter.java
+++ src/main/java/pt/base/incubator/numeric/fitting/PolynomialFitter.java
@@ -1,6 +1,8 @@
 package pt.base.incubator.numeric.fitting;
 
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.apache.commons.math3.analysis.polynomials.PolynomialFunction;
 import org.apache.commons.math3.fitting.PolynomialCurveFitter;
@@ -8,10 +10,12 @@ import org.apache.commons.math3.fitting.WeightedObservedPoints;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
+import com.google.common.primitives.Doubles;
+
 @Service
 public class PolynomialFitter {
 
-	public double[] fit(int degree, Map<Long, Long> observations) {
+	public List<Double> fit(int degree, Map<Long, Long> observations) {
 
 		if (degree <= 0) {
 			throw new IllegalStateException("Polinomial fitter degree must be positive");
@@ -26,14 +30,14 @@ public class PolynomialFitter {
 
 		final PolynomialCurveFitter fitter = PolynomialCurveFitter.create(degree);
 
-		return fitter.fit(obs.toList());
+		return Doubles.asList(fitter.fit(obs.toList()));
 	}
 
-	public double[] error(Map<Long, Long> observations, double[] coeficients) {
+	public List<Double> error(Map<Long, Long> observations, List<Double> coeficients) {
 
-		PolynomialFunction polymonial = new PolynomialFunction(coeficients);
+		PolynomialFunction polymonial = new PolynomialFunction(Doubles.toArray(coeficients));
 
 		return observations.keySet().stream().mapToDouble(value -> observations.get(value) - polymonial.value(value))
-				.toArray();
+				.boxed().collect(Collectors.toList());
 	}
 }
